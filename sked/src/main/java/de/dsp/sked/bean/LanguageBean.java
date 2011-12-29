3baef1769f6a62900cdcb73c75875db9ae3e5d0e
dspiekermann <dennis.spiekermann@gmail.com>
2011-12-29 16:11:36 +0100

    locale

---
+++ sked/src/main/java/de/dsp/sked/bean/LanguageBean.java
@@ -0,0 +1,51 @@
+package de.dsp.sked.bean;
+
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.faces.context.FacesContext;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Scope("session")
+public class LanguageBean {
+
+	private String localeCode;
+
+	private static Map<String, Object> countries;
+	static {
+		countries = new LinkedHashMap<String, Object>();
+		countries.put("English", Locale.ENGLISH); // label, value
+		countries.put("German", Locale.GERMAN);
+	}
+
+	public Map<String, Object> getCountriesInMap() {
+		return countries;
+	}
+
+	public String getLocaleCode() {
+		return localeCode;
+	}
+
+	public void setLocaleCode(String localeCode) {
+		this.localeCode = localeCode;
+	}
+
+	// value change event listener
+	public void countryLocaleCodeChanged() {
+		// loop country map to compare the locale code
+		for (Map.Entry<String, Object> entry : countries.entrySet()) {
+
+			if (entry.getValue().toString().equals(localeCode)) {
+
+				FacesContext.getCurrentInstance().getViewRoot().setLocale((Locale) entry.getValue());
+
+				break;
+			}
+		}
+	}
+	
+}
